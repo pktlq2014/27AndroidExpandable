2d2955e1ce61c2015bb787bfc8ddf77b157d87aa
unknown <nguyenvanty10987654321000@gmail.com>
2019-12-17 15:18:28 +0700

    Edit

--- app/src/main/java/com/example/a27androidexpandable/MainActivity.java
+++ app/src/main/java/com/example/a27androidexpandable/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.a27androidexpandable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,6 +25,71 @@ public class MainActivity extends AppCompatActivity
         customExpandableListView = new CustomExpandableListView(MainActivity.this, arrayList, hashMap);
         expandableListView.setAdapter(customExpandableListView);
         customExpandableListView.notifyDataSetChanged();
+
+        click_Group();
+        click_Child();
+        // sự kiện đóng
+        close_Group();
+        // sự kiện mở
+        open_Group();
+    }
+
+    private void click_Child()
+    {
+       expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener()
+       {
+           @Override
+           public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id)
+           {
+               Toast.makeText(MainActivity.this, hashMap.get(arrayList.get(groupPosition)).get(childPosition), Toast.LENGTH_SHORT).show();
+               return false;
+           }
+       });
+    }
+
+    private void open_Group()
+    {
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener()
+        {
+            @Override
+            public void onGroupExpand(int groupPosition)
+            {
+                Toast.makeText(MainActivity.this, arrayList.get(groupPosition) + " Mở", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void close_Group()
+    {
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener()
+        {
+            @Override
+            public void onGroupCollapse(int groupPosition)
+            {
+                Toast.makeText(MainActivity.this, arrayList.get(groupPosition) + " Đóng", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void click_Group()
+    {
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener()
+        {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id)
+            {
+                expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener()
+                {
+                    @Override
+                    public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id)
+                    {
+                        Toast.makeText(MainActivity.this, arrayList.get(groupPosition), Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+                return false;
+            }
+        });
     }
 
     private void addControl()
